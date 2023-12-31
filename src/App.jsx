14e8eff5d3974abfe284b0133cebc185fce95fc8
Moni-Yahwehson <moniyahwehson@gmail.com>
2023-12-31 18:31:26 +0100

    Working on the heading UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -19,7 +14,7 @@ function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading>The Wild Oasis</Heading>
         <Button onClick={() => alert("Check in")}>Check in</Button>
         <Button onClick={() => alert("Check out")}>Check out</Button>
 
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,12 @@
+import styled, { css } from "styled-components";
+const test = css`
+  text-align: center;
+  ${10 > 5 && "  background-color: yellow"}
+`;
+
+const Heading = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export default Heading;
