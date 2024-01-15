3bb76d964ed651cf5d54ab6d80139b644a1c8d07
Moni-Yahwehson <moniyahwehson@gmail.com>
2024-01-15 13:22:15 +0100

    Working on the modal window

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import CreateCabinForm from "./CreateCabinForm";
+import Button from "../../ui/Button";
+
+function AddCabin() {
+  const [showForm, setShowForm] = useState(false);
+
+  return (
+    <div>
+      <Button onClick={() => setShowForm((show) => !show)}>
+        Add new cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
+    </div>
+  );
+}
+
+export default AddCabin;
