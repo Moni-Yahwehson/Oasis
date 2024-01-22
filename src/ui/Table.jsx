bfc7b9b50fd65583a6e55d142ef8d0e01c84212d
Moni-Yahwehson <moniyahwehson@gmail.com>
2024-01-22 08:32:32 +0100

    Applied the render prop to the reusable table

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -87,7 +87,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data tp show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
